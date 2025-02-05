0a1f3a754164eb0504e3176ccd11e91ebf8da292
Athar Ibrahim Khalid <aikpak24@gmail.com>
2025-02-05 18:07:52 +0500

    Add QR Scanner

---
+++ desktop_mouse/lib/presentation/splash_screen.dart
@@ -0,0 +1,95 @@
+import 'package:desktop_mouse/presentation/home_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MyHomePage(title: "Desktop Mouse Controller"),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Remove the backgroundColor property
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF1C1E33),
+              Color(0xFF4F5B7B),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(flex: 4),
+              Image.asset(
+                "assets/logo1.png",
+                height: 200,
+                width: 200,
+              ),
+              const SizedBox(height: 24),
+              const Spacer(flex: 2),
+              const CircularProgressIndicator(
+                strokeWidth: 4,
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  Color(0xFF1C1E33),
+                ),
+                backgroundColor: Colors.white,
+              ),
+              const Spacer(flex: 1),
+              // Text
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Powered by",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  const Text(
+                    "AIK",
+                    style: TextStyle(
+                      color: Color(0xFF1C1E33),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
