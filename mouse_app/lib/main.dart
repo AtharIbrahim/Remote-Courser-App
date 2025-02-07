d895c4f21155fd64d316d72772b959cc7644c180
Athar Ibrahim Khalid <aikpak24@gmail.com>
2025-02-07 19:17:13 +0500

    Add Theme Mode

--- mouse_app/lib/main.dart
+++ mouse_app/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:mouse_app/presentation/splash_screen.dart';
+import 'package:mouse_app/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MouseApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: MouseApp(),
+    ),
+  );
 }
 
 class MouseApp extends StatelessWidget {
